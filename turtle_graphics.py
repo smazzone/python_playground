a91d34e7c0c3ff70375e250186032d74542e21ac
Stefano Mazzone <stefano.mazzone@gmail.com>
2019-11-11 15:03:33 +0100

    modified README

---
+++ turtle_graphics.py
@@ -0,0 +1,16 @@
+import turtle
+
+#it draws a square
+turtle.down()
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+
+turtle.exitonclick()
+
+
